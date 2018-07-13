203928dbe38e83a99f4f0849659af5b2481de1bc
Karl Fischer <fishi0x01@gmail.com>
2018-07-13 13:48:58 +0200

    Allow local config paths

--- ssh_config.go
+++ ssh_config.go
@@ -4,10 +4,49 @@ import (
 
 	//"fmt"
 
+	"bufio"
+	"fmt"
 	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/kevinburke/ssh_config"
 )
+
+func getDefaultConfig() (*ssh_config.Config, error) {
+	fmt.Println("Read config from default")
+	freader, ferr := os.Open(filepath.Join(os.Getenv("HOME"), ".ssh", "ssh_config_example"))
+	if ferr != nil {
+		panic(ferr)
+	}
+	return readSSHConfig(bufio.NewReader(freader))
+}
+
+func getConfigFromURL() (*ssh_config.Config, error) {
+	resp, herr := http.Get(goSTMUI.ConfigField.Text())
+	if herr != nil {
+		panic(herr)
+	}
+	defer resp.Body.Close()
+	cfgBytes, rerr := ioutil.ReadAll(resp.Body)
+	if rerr != nil {
+		panic(rerr)
+	}
+	cfg := string(cfgBytes)
+	fmt.Println(cfg)
+	return readSSHConfig(strings.NewReader(cfg))
+}
+
+func getConfigFromLocalPath() (*ssh_config.Config, error) {
+	freader, ferr := os.Open(goSTMUI.ConfigField.Text())
+	if ferr != nil {
+		panic(ferr)
+	}
+	return readSSHConfig(bufio.NewReader(freader))
+}
 
 func readSSHConfig(cfgFile io.Reader) (*ssh_config.Config, error) {
 	cfg, err2 := ssh_config.Decode(cfgFile)
